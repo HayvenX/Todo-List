972e009483506a83610dcee7811c4eacb76a170e
HayvenX <pg766282@gmail.com>
2023-12-24 14:50:34 +0200

    Edit & Cancel functions

--- script.js
+++ script.js
@@ -5,26 +5,86 @@ const Input = document.getElementById("input")
 
 function AddtoList()
 {
-    let ListItem = document.createElement("li")
+    const listBlock = document.createElement("div")
+    listBlock.style.marginBottom = "10px"
+    listBlock.style.display = "flex"
+    listBlock.style.cursor = "text"
+    list.appendChild(listBlock)
+
+
+    const ListItem = document.createElement("li")
     ListItem.innerText = Input.value
-    list.appendChild(ListItem)
+    ListItem.style.width = "300px"
+    listBlock.appendChild(ListItem)
+    
     Input.value = ""
 
-        
-    ListItem.onclick = function()
+    listBlock.onclick = function()
     {
-        ListItem.style.textDecoration = "line-through"
-        function DeleteChild()
+        this.savedText = ListItem.innerText
+        if(listBlock.childElementCount == 1)
         {
-            list.removeChild(ListItem)
+            let editing = document.createElement("input")
+            editing.value = ListItem.innerText
+            editing.style.width = "300px"
+            editing.style.backgroundColor = "indigo"
+            editing.style.color = "white"
+            editing.style.border = "none"
+            editing.style.outline = "none"
+            editing.style.fontSize = "24px"
+            editing.setAttribute("autofocus", "")
+            listBlock.appendChild(editing)
+            
+
+            const tick = document.createElement("div")
+            tick.style.width = "33px"
+            tick.style.height = "33px"
+            tick.style.backgroundColor = "limegreen"
+            tick.style.marginRight = "10px"
+            tick.style.cursor = "pointer"
+            listBlock.appendChild(tick)
+        
+            const cancel = document.createElement("div")
+            cancel.style.width = "33px"
+            cancel.style.height = "33px"
+            cancel.style.backgroundColor = "firebrick"
+            cancel.style.cursor = "pointer"
+            listBlock.appendChild(cancel)
+            
+            tick.onclick = function()
+            {
+                listBlock.removeChild(tick)
+                setTimeout(function DeleteCancel()
+                {
+                    cancel.remove()
+                }
+                )
+
+                listBlock.appendChild(ListItem)
+                ListItem.innerText = editing.value
+                editing.remove()
+            }
+
+            cancel.onclick = function()
+            {
+                tick.remove()
+                cancel.remove()
+                setTimeout(function RemoveEditing()
+                {
+                    editing.remove()
+                })
+        
+                listBlock.appendChild(ListItem)
+                ListItem.innerText = listBlock.savedText
+            }
+            listBlock.removeChild(ListItem)
         }
-        setTimeout(DeleteChild, 1000)
     }
 }
 
 function clearList()
 {
-    while(list.childNodes.length > 0)
+    while(list.childElementCount > 0)
     {
         list.removeChild(list.firstChild)
     }
@@ -32,16 +92,11 @@ function clearList()
 
 AddToList.onclick = function()
 {
-    if(Input.value == "")
-    {
-        
-    }
-    else
+    if(Input.value !== "")
     {
         AddtoList()
     }
 }
-
 ClearList.onclick = function()
 {
     clearList()
